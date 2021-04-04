f5c1fc3df6c630cf993da9f42daae65235cc4d7c
Kenny Lee <kennyklee@gmail.com>
2021-04-04 08:06:24 +0000

    script: deploy and publish

--- src/components/bio.js
+++ src/components/bio.js
@@ -36,7 +36,7 @@ const Bio = () => {
         className="bio-avatar"
         layout="fixed"
         formats={["AUTO", "WEBP", "AVIF"]}
-        src="../images/profile_hawaii_small.jpeg"
+        src="../images/profile_hawaii_small.jpg"
         width={50}
         height={50}
         quality={95}
