fbfc32b2f7efdf2b9e629d8af31d71058574eae9
Kenny Lee <kennyklee@gmail.com>
2021-04-07 18:07:02 +0000

    Change footer text

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,9 +3,15 @@ import * as React from "react"
 const Footer = () => {
   return (
     <footer>
-      © {new Date().getFullYear()}, Built with
+      © {new Date().getFullYear()}. Built with
       {` `}
       <a href="https://www.gatsbyjs.com">Gatsby</a>
+      {` `}
+      using
+      {` `}
+      <a href="https://gitpod.io" target="_blank">Gitpod.io</a> and
+      {` `}
+      <a href="https://github.com/kennyklee/blog/projects/1" target="_blank">Github.</a>
     </footer>
   )
 }
