2463b299059e12696937c251ed6d3d49ea2de2aa
Kenny Lee <kennyklee@gmail.com>
2021-04-07 06:35:46 +0000

    Update homepage

--- src/pages/index.js
+++ src/pages/index.js
@@ -14,7 +14,9 @@ const HomePage = ({ data, location }) => {
   <Layout location={location} title={siteTitle}>
     <SEO title="All posts" />
     <Bio />
-    <p>I should say something clevers about myself here. and render markdown page here.</p>
+    <b>April 2021</b>
+    <p>I am currently learning how to create this site using <a href="https://gatsbyjs.com" target="_blank">Gatsby</a> and <a href="https://reactjs.org" target="_blank">React</a>.</p>
+    <p>I'm using <a href="https://gitpod.io" target="_blank">Gitpod.io</a> as a cloud IDE, and <a href="https://github.com/kennyklee/blog/projects/1" target="_blank">Github</a> to serve static pages. Not having a local development environment on a computer and using <a href="https://code.visualstudio.com/" target="_blank">VSCode</a> to code in the cloud is amazing.</p>
   </Layout>
   )
 }
