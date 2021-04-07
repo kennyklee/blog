8fc20a52685b3d95201fdbd018b223e4debb4cfc
Kenny Lee <kennyklee@gmail.com>
2021-04-07 05:39:36 +0000

    Update Footer path

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -1,6 +1,6 @@
 import * as React from "react"
 import { Link } from "gatsby"
-import Footer from "../components/Footer"
+import Footer from "./Footer"
 
 const Layout = ({ location, title, children }) => {
   const rootPath = `${__PATH_PREFIX__}/`
