8ba5541304eb86ddfcb784340126f40f194e8a10
Kenny Lee <kennyklee@gmail.com>
2021-04-09 23:34:25 -0700

    Update favicon - fixes #14

--- gatsby-config.js
+++ gatsby-config.js
@@ -112,13 +112,13 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: `Gatsby Starter Blog`,
-        short_name: `GatsbyJS`,
+        name: `Kenny Lee Blog`,
+        short_name: `Kenny Lee`,
         start_url: `/`,
         background_color: `#ffffff`,
         theme_color: `#663399`,
         display: `minimal-ui`,
-        icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
+        icon: `src/images/shaka.png`, // This path is relative to the root of the site.
       },
     },
     `gatsby-plugin-react-helmet`,
